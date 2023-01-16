9be0ee1b00344fca789dc9693354d795fed7932f
djakimiuk <djakimiuk@outlook.com>
2023-01-17 00:12:39 +0100

    1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const FeedbackHandle = ({ handleClick, text }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <button onClick={handleClick}>{text}</button>
+    </>
+  );
+};
+
+const FeedbackDisplay = ({ text, counter }) => {
+  return (
+    <>
+      <p>
+        {text} {counter}
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <FeedbackHandle handleClick={() => setGood(good + 1)} text={"good"} />
+      <FeedbackHandle
+        handleClick={() => setNeutral(neutral + 1)}
+        text={"neutral"}
+      />
+      <FeedbackHandle handleClick={() => setBad(bad + 1)} text={"bad"} />
+      <h1>statistics</h1>
+      <FeedbackDisplay text="good" counter={good} />
+      <FeedbackDisplay text="neutral" counter={neutral} />
+      <FeedbackDisplay text="bad" counter={bad} />
     </div>
   );
-}
+};
 
 export default App;
