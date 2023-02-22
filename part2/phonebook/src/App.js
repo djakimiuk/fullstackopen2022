a32a017dc4e202996dd1c3e10f093d7846041a29
djakimiuk <djakimiuk@outlook.com>
2023-02-22 19:37:19 +0100

    2.11: The Phonebook -Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterValue, setFilterValue] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -55,12 +57,23 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      
-      <Filter filterValue={filterValue} setFilterValue={setFilterValue} persons={persons} handleFilterValueChange={handleFilterValueChange} />
+
+      <Filter
+        filterValue={filterValue}
+        setFilterValue={setFilterValue}
+        persons={persons}
+        handleFilterValueChange={handleFilterValueChange}
+      />
       <h2>add a new</h2>
-      <PersonForm handleSubmit={handleSubmit} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+      <PersonForm
+        handleSubmit={handleSubmit}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      <Persons namesToShow={namesToShow}/>
+      <Persons namesToShow={namesToShow} />
     </div>
   );
 };
